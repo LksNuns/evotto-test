ee8610cb1f90c3d7514c046ff9327da6e247522f
Lucas Arvani <lksnunss@gmail.com>
2018-03-17 14:58:20 -0300

    Garantindo apenas uma opção de filtro

--- lib/cli.rb
+++ lib/cli.rb
@@ -13,14 +13,37 @@ class CLI
   #
   def parse!
     begin
-      result = Slop::Parser.new(opt_parser).parse(@args)
-      result.to_hash
-    rescue Slop::UnknownOption => e
+      result = Slop::Parser.new(opt_parser).parse(@args).to_hash
+      ensure_one_option(result)
+      result
+    rescue Exception => e
       raise
     end
   end
 
   private
+  #
+  # Recebe resultado de `parse` e garante que existe
+  # apenas uma _flag_ de `option`
+  #
+  # @param [Hash] Hash com resultados de parse
+  #
+  def ensure_one_option(result)
+    options = %i[order_by total find]
+    result.delete_if { |k, v| v.nil? || (v == []) }
+    result[:option] = nil
+
+    # Cria um array com todos os `options` que `result` tem
+    options_keys = result.keys.reject { |k| !options.include? k }
+
+    # Caso exista mais de uma opção setada
+    raise ArgumentError, "More than one option" if options_keys.size > 1
+
+    option = options_keys.first
+    result[:option] = { method: option, args: result[option] } if options_keys.any?
+
+    result
+  end
 
   #
   # Opções de parametros para CLI
--- spec/lib/cli_spec.rb
+++ spec/lib/cli_spec.rb
@@ -3,15 +3,23 @@ require "spec_helper"
 RSpec.describe CLI do
   describe "#parse" do
     let(:args) { ['--source', 'data.csv', '--order_by', 'age', 'desc'] }
+    let(:cli) { CLI.new(args)}
 
     describe "When passing valid args" do
-      let(:cli) { CLI.new(args)}
-
       it "returns a hash with values" do
         expect(cli.parse!).to include({
-          :source=>"data.csv",
-          :order_by=>["age", "desc"]
+          source: "data.csv",
+          option: { method: :order_by, args: ["age", "desc"] }
         })
+      end
+    end
+
+    describe "When passing many options" do
+      # Passando --order_by e --find
+      let(:args) { ['--source', 'data.csv', '--order_by', 'age', 'desc', '--find', 'john'] }
+
+      it "raise an error" do
+        expect{ cli.parse! }.to raise_error ArgumentError
       end
     end
 
